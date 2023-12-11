24e21f4cf9efac8c147217426d16a7b7992b73b0
AroojAslam <aroojaslam25@gmail.com>
2023-12-11 17:42:28 +0500

    add category

---
+++ app/src/main/java/com/example/doctorappointmentsystem/CategorieAdapter.kt
@@ -0,0 +1,35 @@
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.doctorappointmentsystem.R
+
+class CategorieAdapter(private val dataList: List<String>, private val icons: List<String>) : RecyclerView.Adapter<CategorieAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_horizontal_list, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(dataList[position], icons[position])
+    }
+
+    override fun getItemCount(): Int {
+        return dataList.size
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val imageView: ImageView = itemView.findViewById(R.id.icons)
+        private val textView: TextView = itemView.findViewById(R.id.textItem)
+
+        fun bind(item: String, icon: String) {
+            textView.text = item
+
+            val resourceId = itemView.context.resources.getIdentifier(icon, "drawable", itemView.context.packageName)
+            imageView.setImageResource(resourceId)
+        }
+    }
+}
--- app/src/main/java/com/example/doctorappointmentsystem/HomePage.kt
+++ app/src/main/java/com/example/doctorappointmentsystem/HomePage.kt
@@ -1,14 +1,15 @@
 package com.example.doctorappointmentsystem
 
+import CategorieAdapter
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
-import android.view.Menu
-import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class HomePage : Fragment() {
 
@@ -21,12 +22,17 @@ class HomePage : Fragment() {
         val toolbar: Toolbar = view.findViewById(R.id.toolbar)
         (activity as AppCompatActivity?)?.setSupportActionBar(toolbar)
         (activity as AppCompatActivity?)?.supportActionBar?.title = "Doctor Appointment"
+        val DocType = listOf("All", "Cardiologist", "Dentist")
+        val icons = listOf("@drawable/logo", "@drawable/ic_heart", "@drawable/ic_tooth")
+        val recyclerView: RecyclerView = view.findViewById(R.id.horizontalRecyclerView)
+        val layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        recyclerView.layoutManager = layoutManager
+
+        val adapter = CategorieAdapter(DocType,icons)
+        recyclerView.adapter = adapter
         return view
     }
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(R.menu.more_menu, menu)
 
-    }
 
 
 }
